5eb929a6dca9dfe3b88a4af305bbdfb55632fe53
Evina Mouselimi <evinamous@gmail.com>
2024-01-16 19:42:49 +0200

    abstract class Vehicle

--- objects.php
+++ objects.php
@@ -1,6 +1,17 @@
 <?php 
 
-class Vehicle {
+abstract class Vehicle {
+
+    var $make;
+    var $model;
+    var $year;
+    var $mileage;
+    var $price;
+    var $image;
+
+}
+
+class Car extends Vehicle{
 
     function __construct($make, $model, $year, $mileage, $price, $image) {
         $this->make = $make;
@@ -10,27 +21,25 @@ class Vehicle {
         $this->price = $price;
         $this->image = $image;
     }
-
-    var $make;
-    var $model;
-    var $year;
-    var $mileage;
-    var $price;
-    var $image;
 }
 
 class Truck extends Vehicle {
     function __construct($make, $model, $year, $mileage, $price, $image, $engine) {
-        Vehicle::__construct($make, $model, $year, $mileage, $price, $image);
+        $this->make = $make;
+        $this->model = $model;
+        $this->year = $year;
+        $this->mileage = $mileage;
+        $this->price = $price;
+        $this->image = $image;
         $this->engine = $engine;
     }
 
     var $engine;
 }
 
-$vehicle1 = new Vehicle("Astro", "Estrella", "2021", 500, 50000, "veh-01.jpg");
-$vehicle2 = new Vehicle("Terraza", "Spinneo", "2020", 30000, 31000, "veh-02.jpg");
-$vehicle3 = new Vehicle("Sage", "Ecostar", "2014", 70000, 15000, "veh-03.jpg");
+$vehicle1 = new Car("Astro", "Estrella", "2021", 500, 50000, "veh-01.jpg");
+$vehicle2 = new Car("Terraza", "Spinneo", "2020", 30000, 31000, "veh-02.jpg");
+$vehicle3 = new Car("Sage", "Ecostar", "2014", 70000, 15000, "veh-03.jpg");
 $vehicle4 = new Truck("Hauler", "Lion", "2021", 200, 40000, "veh-04.jpg", "diesel");
 
 $vehicles = array($vehicle1, $vehicle2, $vehicle3, $vehicle4);
